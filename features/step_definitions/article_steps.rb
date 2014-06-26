fe77b8e1c53c012f3870c687f086332c67136ad7
RockyKnowledge <rocco.sapienza@gmail.com>
2014-06-26 10:55:11 -0300

    HW1

---
+++ features/step_definitions/article_steps.rb
@@ -0,0 +1,16 @@
+Given /the following (.*?) exist:$/ do |type, table|
+  table.hashes.each do |element|
+    if type == "users" then User.create(element)
+    elsif type == "articles" then Article.create(element)
+    elsif type == "comments" then Comment.create(element)
+    end
+  end
+end
+
+Given /I am logged in as "(.*?)" with pass "(.*?)"$/ do |user, pass|
+  visit '/accounts/login'
+  fill_in 'user_login', :with => user
+  fill_in 'user_password', :with => pass
+  click_button 'Login'
+  assert page.has_content? 'Login successful'
+end
